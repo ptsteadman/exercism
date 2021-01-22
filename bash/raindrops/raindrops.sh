a954300bd6a6d7db529b5ca7c5cab630d353e04e
Patrick Steadman <ptsteadman@gmail.com>
2021-01-22 13:15:30 -0500

    improve raindrops with arithmetic conditional

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -11,19 +11,17 @@ fi
 
 has_factor=false
 
-if [[ $(($1 % 3)) -eq 0 ]]; then
+if (($1 % 3 == 0)); then
   printf 'Pling'
   has_factor=true
 fi
-if [[ $(($1 % 5)) -eq 0 ]]; then
+if (($1 % 5 == 0)); then
   printf 'Plang'
   has_factor=true
 fi
-if [[ $(($1 % 7)) -eq 0 ]]; then
+if (($1 % 7 == 0)); then
   printf 'Plong'
   has_factor=true
 fi
 
-if [[ "$has_factor" = false ]]; then
-  echo $1
-fi
+$has_factor || echo "$1"
