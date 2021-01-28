13386c0e4f3f3264100fec89fd62fd67936ea5af
Patrick Steadman <ptsteadman@gmail.com>
2021-01-28 18:02:00 -0500

    finish acronym
    Signed-off-by: Patrick Steadman <ptsteadman@gmail.com>

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
+# learnings:
+# IFS can be more than one character
+# IFS prepended to command applies only to that command
+# <<< redirection operator is called a "here string" does not perform filename
+# expansion or word splitting
+# read command is used to retrieve data from stdin aka file descriptor 0 
+# read foo reads data from stdin to variable foo
+# read -a flag reads lines into an array
+# read -d flag specifies the "line" delimiter
+# for in array loop syntax
 
-read -ra words -d '' <<<"$1"
+IFS='- ' read -a words -d '' <<< "$1"
 
 for word in "${words[@]}"; do
   stripped=${word//[^a-zA-Z]/}
   first_char=${stripped:0:1}
   if ! [[ -z $first_char ]]; then 
-    printf $(tr '[a-z]' '[A-Z' <<< $first_char)
+    printf $(tr '[a-z]' '[A-Z]' <<< $first_char)
   fi
 done
 
