e146e820dab4079c0d29205a496e09594ace4fad
Patrick Steadman <ptsteadman@gmail.com>
2021-08-07 23:13:32 -0400

    Finish up Matrix
    Signed-off-by: Patrick Steadman <ptsteadman@gmail.com>

--- go/matrix/matrix.go
+++ go/matrix/matrix.go
@@ -6,14 +6,14 @@ import (
 	"strings"
 )
 
-type Matrix struct {
-	values [][]int
-}
+// Matrix is a m*n two dimensional slice of ints with Rows, Cols and Set methods
+type Matrix [][]int
 
-func New(s string) (*Matrix, error) {
+// New returns a new Matrix
+func New(s string) (Matrix, error) {
 	m := Matrix{}
 	rows := strings.Split(s, "\n")
-	m.values = make([][]int, len(rows))
+	m = make([][]int, len(rows))
 	var numCols int
 	for i, row := range rows {
 		stringRowValues := strings.Split(strings.TrimSpace(row), " ")
@@ -31,42 +31,46 @@ func New(s string) (*Matrix, error) {
 			}
 			rowValues[c] = intVal
 		}
-		m.values[i] = rowValues
+		m[i] = rowValues
 	}
-	return &m, nil
+	return m, nil
 }
 
-func (m *Matrix) Rows() [][]int {
-	rowsCopy := make([][]int, len(m.values))
-	for i := range m.values {
-		rowsCopy[i] = make([]int, len(m.values[i]))
-		copy(rowsCopy[i], m.values[i])
+// Rows returns a two dimensional row-wise copy of Matrix values
+func (m Matrix) Rows() [][]int {
+	rowsCopy := make([][]int, len(m))
+	for i := range m {
+		rowsCopy[i] = make([]int, len(m[i]))
+		copy(rowsCopy[i], m[i])
 	}
 	return rowsCopy
 }
 
-func (m *Matrix) Cols() [][]int {
-	if len(m.values) == 0 {
+// Cols returns a two dimensional column-wise copy of Matrix values
+func (m Matrix) Cols() [][]int {
+	if len(m) == 0 {
 		return make([][]int, 0)
 	}
-	numCols := len(m.values[0])
+	numCols := len(m[0])
 	colsCopy := make([][]int, numCols)
 	for i := 0; i < numCols; i++ {
-		colsCopy[i] = make([]int, len(m.values))
-		for j, row := range m.values {
+		colsCopy[i] = make([]int, len(m))
+		for j, row := range m {
 			colsCopy[i][j] = row[i]
 		}
 	}
 	return colsCopy
 }
 
-func (m *Matrix) Set(r int, c int, v int) bool {
-	if r < 0 || r >= len(m.values) {
+// Set modifies a Matrix value at the specified row r, columni c, to a new value v
+// Returns false if rows and columns were out of range, otherwise returns true
+func (m Matrix) Set(r int, c int, v int) bool {
+	if r < 0 || r >= len(m) {
 		return false
 	}
-	if c < 0 || c >= len(m.values[0]) {
+	if c < 0 || c >= len(m[0]) {
 		return false
 	}
-	m.values[r][c] = v
+	m[r][c] = v
 	return true
 }
