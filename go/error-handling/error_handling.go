e4e152272532d6ece3e52f70f5fd8bb6bba0a73a
Patrick Steadman <ptsteadman@gmail.com>
2021-08-12 14:09:18 -0400

    Clean up error handling

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -1,16 +1,28 @@
 package erratum
 
+import "fmt"
+
+// openWithRetry attempts to open resource o. If an error of type
+// TransientError is returned, it continually retries until it can
+// successfully return the resource or another type of error is returned
 func openWithRetry(o ResourceOpener) (Resource, error) {
-	resource, err := o()
-	if err == nil {
-		return resource, nil
-	}
-	if _, ok := err.(TransientError); ok {
-		return openWithRetry(o)
+	var resource Resource
+	var err error
+	for err == nil {
+		resource, err = o()
+		if err == nil {
+			return resource, nil
+		}
+		if _, ok := err.(TransientError); ok {
+			// if the error is a TransientError, try again
+			err = nil
+		}
 	}
 	return nil, err
 }
 
+// Use opens a resource, Frobs a string to it, and ensures that the
+// resource is appropriately closed and defrobbed if necessary
 func Use(o ResourceOpener, input string) (err error) {
 	resource, err := openWithRetry(o)
 	if err != nil {
@@ -19,12 +31,14 @@ func Use(o ResourceOpener, input string) (err error) {
 	defer resource.Close()
 	defer func() {
 		if r := recover(); r != nil {
-			if e, ok := r.(error); ok {
+			switch e := r.(type) {
+			case FrobError:
+				resource.Defrob(e.defrobTag)
+				err = e.inner
+			case error:
 				err = e
-			}
-			if frobError, ok := r.(FrobError); ok {
-				resource.Defrob(frobError.defrobTag)
-				err = frobError.inner
+			default:
+				err = fmt.Errorf("%s", r)
 			}
 		}
 	}()
